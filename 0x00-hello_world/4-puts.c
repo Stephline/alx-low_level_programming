d0e714ca5a47e7d9e971033b598fa8d891635352
Steph <stephanieasante12@gmail.com>
2023-04-23 22:36:50 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<<stdio.h>
-
 /**
- * main - Entry point
+ * main - Print a line of code using puts
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
         return (0);
 } 
